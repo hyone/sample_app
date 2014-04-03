53cb1ca265e74c23b0a83c699f9089807a3d25c5
hyone <hyone.development@gmail.com>
2014-04-03 21:47:18 +0900

    Refactor specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,65 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe 'StaticPages' do
   subject { page }
 
-  let (:base_title) { 'Ruby on Rails Tutorial Sample App' }
+  shared_examples_for 'all static pages' do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
-  describe "Home page" do
+  describe 'Home page' do
     before { visit root_path }
+    let (:heading) { 'Sample App' }
+    let (:page_title) { '' }
 
-    it { should have_content('Sample App') }
-
-    it { should have_title(full_title('')) }
-
+    it_should_behave_like 'all static pages'
     it { should_not have_title('| Home') }
   end
 
-  describe "Help page" do
+  describe 'Help page' do
     before { visit help_path }
+    let (:heading) { 'Help' }
+    let (:page_title) { 'Help' }
 
-    it { should have_content('Help') }
-
-    it { have_title(full_title('Help')) }
+    it_should_behave_like 'all static pages'
   end
 
-  describe "About page" do
+  describe 'About page' do
     before { visit about_path }
 
-    it { should have_content('About Us') }
+    let (:heading) { 'About Us' }
+    let (:page_title) { 'About' }
 
-    it { should have_title(full_title('About')) }
+    it_should_behave_like 'all static pages'
   end
 
-  describe "Contact" do
+  describe 'Contact' do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    let (:heading) { 'Contact' }
+    let (:page_title) { 'Contact' }
 
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like 'all static pages'
+  end
+
+  it "have the right links on the layout" do
+    visit root_path
+
+    click_link 'About'
+    should have_title(full_title('About Us'))
+
+    click_link 'Help'
+    should have_title(full_title('Help'))
+
+    click_link 'Contact'
+    should have_title(full_title('Contact'))
+
+    click_link 'Home'
+    click_link "Sign up now!"
+    should have_title(full_title('Sign up'))
+
+    click_link 'sample app'
+    should have_title(full_title(''))
   end
 end
